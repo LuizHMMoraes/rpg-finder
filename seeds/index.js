6c251af37c7807c9c44eb9a39373705af92000e1
Luiz Marques <luizm.archer@gmail.com>
2023-10-11 17:04:38 -0300

    Seeds fix

--- seeds/index.js
+++ seeds/index.js
@@ -22,8 +22,8 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
 
-        const camp = new RpgGroup({
-            author: '64a47031b294cdaf028d0b32',
+        const group = new RpgGroup({
+            author: '65219aae81b8dbb35ea8c71b',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto libero inventore, consequatur harum, itaque sunt fugiat assumenda delectus odit quam dicta fugit possimus est optio dolores eum, provident laboriosam eos?',
@@ -37,16 +37,14 @@ const seedDB = async () => {
             },
             images: [
                 {
+                    url: 'https://res.cloudinary.com/djxomm4es/image/upload/v1696780110/Rpg%20Finder/istockphoto-1181398275-170667a_pt5fmi.webp',
+                    filename: 'Rpg Finder/istockphoto-1181398275-170667a_pt5fmi'
                     // url: 'https://res.cloudinary.com/djxomm4es/image/upload/v1690486784/Yelp%20Camp/y4pvj5rvsfkapusqux6c.jpg',
-                    filename: 'Rpg Finder/y4pvj5rvsfkapusqux6c'
-                },
-                {
-                    // url: 'https://res.cloudinary.com/djxomm4es/image/upload/v1690486784/Yelp%20Camp/ioykjofiaq62mgzaenvz.jpg',
-                    filename: 'Rpg Finder/ioykjofiaq62mgzaenvz'
+                    // filename: 'Rpg Finder/istockphoto-1181398275-170667a_pt5fmi'
                 }
             ]
         });
-        await camp.save();
+        await group.save();
     }
 }
 
